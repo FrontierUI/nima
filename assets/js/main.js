5028dd6101d37c443fa010643ee1feee6f1475df
FrontierUI <uifrontier95@gmail.com>
2025-02-23 14:02:38 +0500

    issue resolved

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,47 @@
 // Navbar
 
+const sidebar = document.getElementById('sidebar');
+const openSidebarButton = document.getElementById('open-sidebar');
+const collapseMenu = document.getElementById('collapseMenu');
+
+openSidebarButton.addEventListener('click', (e) => {
+  e.stopPropagation();
+  sidebar.classList.toggle('-translate-x-full');
+  setTimeout(() => {
+    document.querySelector('body').classList.add('overflow-hidden');
+  }, 50);
+});
+
+collapseMenu.addEventListener('click', (e) => {
+  e.stopPropagation();
+  sidebar.classList.toggle('-translate-x-full');
+});
+
+document.addEventListener('click', (e) => {
+  if (!sidebar.contains(e.target) && !openSidebarButton.contains(e.target)) {
+    sidebar.classList.add('-translate-x-full');
+  }
+});
+
+document.addEventListener('click', (e) => {
+  if (!sidebar.contains(e.target) && !collapseMenu.contains(e.target)) {
+    sidebar.classList.add('-translate-x-full');
+  }
+});
+
+document.getElementById('btnClose').addEventListener('click', () => {
+  sidebar.classList.add('-translate-x-full');
+  document.querySelector('body').classList.remove('overflow-hidden');
+});
+
+document.getElementById('btnSMClose').addEventListener('click', () => {
+  sidebar.classList.add('-translate-x-full');
+  document.querySelector('body').classList.remove('overflow-hidden');
+});
+
+//
+// Slickkks Sliders
+
 $('.treatSlider').slick({
   dots: false,
   arrows: false,
